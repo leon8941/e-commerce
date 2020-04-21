3161f708db257154070b9e2bde9975f8595f5ead
Teck Onn <teckonn@wobb.co>
2020-04-22 01:59:34 +0800

    add role to user

--- adminBro/index.js
+++ adminBro/index.js
@@ -5,9 +5,47 @@ const db = require('../models/index')
 
 const getAdminBro = () => {
   AdminBro.registerAdapter(AdminBroSequelize)
-  
+
+  let adminUser = {
+    resource: db.AdminUser,
+    options: {
+      properties: {
+        encryptedPassword: {
+          isVisible: false,
+        },
+        password: {
+          type: 'string',
+          isVisible: {
+            list: false, edit: true, filter: false, show: false,
+          },
+        },
+        role: {
+          availableValues: [
+            {value: 'admin', label: 'Admin'},
+            {value: 'marketing', label: 'Marketing'},
+            {value: 'finance', label: 'Finance'}, 
+          ]
+        }
+      },
+      actions: {
+        new: {
+          before: async (request) => {
+            if(request.payload.record.password) {
+              request.payload.record = {
+                ...request.payload.record,
+                encryptedPassword: await bcrypt.hash(request.payload.record.password, 10),
+                password: undefined,
+              }
+            }
+            return request
+          },
+        }
+      }
+    }
+  }
+
   const adminBroOptions = {
-    resources: [db.AdminUser],
+    resources: [adminUser],
     rootPath: '/admin',
   }
 
--- models/adminuser.js
+++ models/adminuser.js
@@ -3,7 +3,8 @@ module.exports = (sequelize, DataTypes) => {
   const AdminUser = sequelize.define('AdminUser', {
     email: DataTypes.STRING,
     name: DataTypes.STRING,
-    encryptedPassword: DataTypes.STRING
+    encryptedPassword: DataTypes.STRING,
+    role: DataTypes.STRING
   }, {})
   AdminUser.associate = function(models) {
     // associations can be defined here
