e0456836b673bdbc71297af5f74e77d8d1972953
Teck Onn <teckonn@wobb.co>
2020-04-22 01:19:09 +0800

    tweaks

--- models/adminuser.js
+++ models/adminuser.js
@@ -3,7 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   const AdminUser = sequelize.define('AdminUser', {
     email: DataTypes.STRING,
     name: DataTypes.STRING,
-    password: DataTypes.STRING
+    encryptedPassword: DataTypes.STRING
   }, {})
   AdminUser.associate = function(models) {
     // associations can be defined here
